f86dbb1542c927a961449ca80089f8c9fb1fa3e0
Vrushabhpatel <60414314+Vrushabhpatel@users.noreply.github.com>
2020-01-28 23:16:16 -0500

    Add files via upload

---
+++ Binary_Converter_Program.cs
@@ -0,0 +1,18 @@
+﻿using Converter.Models;
+using System;
+
+namespace Binaryconverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Vrushabh Patel";
+
+            BinaryConverter binaryConverter = new BinaryConverter();
+            string binaryValue = binaryConverter.ConvertTo(name);
+            Console.WriteLine($"{name} as Binary: {binaryValue}");
+            Console.WriteLine($"{name} from Binary to ASCII: {binaryConverter.ConvertBinaryToString(binaryValue)}");
+        }
+    }
+}
