2a28bac3ce954a356159a9db7d5d378a715c80ee
Vrushabhpatel <60414314+Vrushabhpatel@users.noreply.github.com>
2020-01-28 23:47:50 -0500

    Add files via upload

---
+++ HexaDecimal_Converter_Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HexaDecimalConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Vrushabh Patel";
+
+            HexadecimalConverter hexadecimalConverter = new HexadecimalConverter();
+            string hexadecimalValue = hexadecimalConverter.ConvertTo(name);
+            Console.WriteLine($"{name} as Hexadecimal: {hexadecimalValue}");
+            Console.WriteLine($"{name} from Hexadecimal to ASCII: {hexadecimalConverter.ConveryFromHexToASCII(hexadecimalValue)}");
+        }
+    }
+}
